b96b5d5555eb1bf3f0ecae753dadbf39a86e2432
F1L1N <pavel9696@yandex.com>
2019-08-06 13:43:23 +0300

    Modify Blog.Core

---
+++ Blog.Core/IBlogRepository .cs
@@ -0,0 +1,11 @@
+﻿using Blog.Core.Objects;
+using System.Collections.Generic;
+
+namespace Blog.Core
+{
+    public interface IBlogRepository
+    {
+        IList<Post> Posts(int pageNo, int pageSize);
+        int TotalPosts();
+    }
+}
